6971c5b0f90b9b68a856733a9a3db6efe6d95d5c
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-14 21:31:05 -0600

    Creando oauth y configurando eureka client

--- src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -2,7 +2,13 @@ package com.formacionbdi.springboot.app.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Clase de arranque de la aplicación SpringBoot.
+ * @EnableEurekaClient: Se Habilita Eureka Client.
+ */
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioOauthApplication {
 
