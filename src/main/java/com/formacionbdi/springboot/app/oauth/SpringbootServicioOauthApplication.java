91e33389bf8fd5251785cb74ab4628f057c849f1
MAALONSO <miguel.coba@gnp.com.mx>
2020-03-14 22:11:02 -0600

    Creando interfaz Feign Client. Implementando servicio UserDatailsService con Feign Client

--- src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/SpringbootServicioOauthApplication.java
@@ -3,11 +3,14 @@ package com.formacionbdi.springboot.app.oauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Clase de arranque de la aplicación SpringBoot.
  * @EnableEurekaClient: Se Habilita Eureka Client.
+ * @EnableFeignClients: Se habilita el cliente Feign.
  */
+@EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
 public class SpringbootServicioOauthApplication {
---
+++ src/main/java/com/formacionbdi/springboot/app/oauth/clients/UsuarioFeignClient.java
@@ -0,0 +1,19 @@
+package com.formacionbdi.springboot.app.oauth.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
+
+/**
+ * Interface UsuarioFeignClient.
+ * Para que sea un cliente Fegn se agrega la etiqueta de @FeignClinet.
+ */
+@FeignClient(name = "servicio-usuarios")
+public interface UsuarioFeignClient {
+	
+	@GetMapping("/usuarios/search/buscar-username")
+	public Usuario findByUsername(@RequestParam String username);
+
+}
