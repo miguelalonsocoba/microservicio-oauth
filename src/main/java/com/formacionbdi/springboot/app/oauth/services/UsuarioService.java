1239dc308d231cac796978e2568a9830ebf04c60
MAALONSO <miguel.coba@gnp.com.mx>
2020-04-19 17:25:00 -0500

    Manejando error 404 en componente de servicio UserDetailsServices

--- src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
+++ src/main/java/com/formacionbdi/springboot/app/oauth/services/UsuarioService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import com.formacionbdi.springboot.app.commons.usuarios.models.entity.Usuario;
 import com.formacionbdi.springboot.app.oauth.clients.UsuarioFeignClient;
 
+import feign.FeignException;
+
 /**
  * Class UsuarioService. Se implemtane una interface propia de Spring Security.
  */
@@ -36,24 +38,26 @@ public class UsuarioService implements IUsuarioService, UserDetailsService {
 	 */
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Usuario usuario = client.findByUsername(username);
 
-		// En caso de que no existe el usuario.
-		if (usuario == null) {
+		try {
+
+			Usuario usuario = client.findByUsername(username);
+
+			List<GrantedAuthority> authorities = usuario.getRoles().stream()
+					.map(role -> new SimpleGrantedAuthority(role.getNombre()))
+					.peek(authority -> log.info(String.format("Role: %s", authority.getAuthority())))
+					.collect(Collectors.toList());
+
+			log.info(String.format("Usuario autenticado: %s", username));
+
+			return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
+					authorities);
+
+		} catch (FeignException e) {
 			log.error(String.format("Error en el login, no existe el usuario %s en el sistema.", username));
 			throw new UsernameNotFoundException(
 					"Error en el login, no existe el usuario '" + username + "' en el sistema.");
 		}
-
-		List<GrantedAuthority> authorities = usuario.getRoles().stream()
-				.map(role -> new SimpleGrantedAuthority(role.getNombre()))
-				.peek(authority -> log.info(String.format("Role: %s", authority.getAuthority())))
-				.collect(Collectors.toList());
-
-		log.info(String.format("Usuario autenticado: %s", username));
-
-		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true,
-				authorities);
 	}
 
 	@Override
